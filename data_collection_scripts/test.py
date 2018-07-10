14e7d9ea440fbd34feec359427002b44f51ad512
Dorel.comigo <dorel.moran@comigo.com>
2018-07-10 11:24:18 +0300

    test commit

--- data_collection_scripts/test.py
+++ data_collection_scripts/test.py
@@ -1,24 +1,64 @@
-import sys
+from google.cloud import bigquery
+import os, sys
 
-if len(sys.argv) >= 2:
+
+def table_query(input_A, input_B, input_where):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = \
+        "C:\\Users\dorel.moran\PycharmProjects\csv_scripts\BigQuery\google_key_credentials.json"
+
+    client = bigquery.Client()
+    dataset_id = 'tests'
+    dataset_ref = client.dataset(dataset_id)
+    table_id = 'test1'
+    table_ref = dataset_ref.table(table_id)
+    table = client.get_table(table_ref)
+    table_root = dataset_id + '.' + table_id
+
+    query = ''
+    if input_A == 'select':
+        query = select_query(input_B, input_where, table_root)
+
+    elif input_A == 'insert':
+        query = insert_query(input_B, table_root)
+
+    print(query)
+    query_job = client.query(query)
+    for row in query_job: print(row)
+
+def select_query(input_B, input_where, table_root):
+    if input_B: pass
+    else: input_B = '*'
+    if input_where ==
+    return ('SELECT ' + input_B
+             + ' FROM ' + table_root)
+
+
+def insert_query(input_B, table_root):
+    return ('SELECT ' + input_B
+            + ' FROM ' + table_root)
+
+
+def update_query():
+    pass
+
+
+def main():
     input_A = sys.argv[1]
-else:
-    input_A = input("Please enter a number for the relevant query to run: "
-                    "\n 1 - SELECT"
-                    "\n 2 - INSERT"
-                    "\n 3 - UPDATE:")
-if input_A == '1':
-    if len(sys.argv) >= 3:
+    input_B = ''
+    input_where = ''
+    if input_A == 'select':
+        if sys.argv[2] != 'where':
+            arg_count = len(sys.argv)
+            for i in range(arg_count - 2): input_B += sys.argv[i + 2] + ', '
+            input_B = input_B[0:-2]  # Remove the last comma and space.
+        else: input_B = ''
+    elif input_A == 'insert':
         input_B = sys.argv[2]
     else:
-        input_B = input("Please enter the name of the requested columns to show:")
-print(input_B)
-print(len(input_B.split(" ")))
+        print('ERROR - First argument should only be "select" or "insert"')
+        exit(1)
+    table_query(input_A, input_B, input_where)
 
 
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
