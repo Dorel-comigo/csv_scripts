14385ef1539db4aa891f42774d793b5ce280309d
Dorel.comigo <dorel.moran@comigo.com>
2018-07-11 11:41:11 +0300

    test commit

--- data_collection_scripts/test.py
+++ data_collection_scripts/test.py
@@ -3,9 +3,10 @@ from formatted_string import formatted_name
 import os, sys, csv
 
 
-def table_query(input_A, input_B):
+def table_query(input_A):
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = \
         "C:\\Users\dorel.moran\PycharmProjects\csv_scripts\BigQuery\google_key_credentials.json"
+#TODO;: put file in GIT repository
 
     client = bigquery.Client()
     dataset_id = 'tests'
@@ -15,57 +16,40 @@ def table_query(input_A, input_B):
     table = client.get_table(table_ref)
     table_root = dataset_id + '.' + table_id
 
-    query = ''
-    if input_A == 'select':
-        query = select_query(input_B, input_where, table_root)
-
-    elif input_A == 'insert':
-        query = insert_query(input_B, table_root)
-
+    query = insert_query(input_A, table_root)
     print(query)
+
     query_job = client.query(query)
     for row in query_job: print(row)
 
-def select_query(input_B, table_root):
-    if input_B: pass
-    else: input_B = '*'
-    #TODO: Add 'WHERE' timestamp column is latest for each item (differentiated by item_id)
-    return ('SELECT ' + input_B
-             + ' FROM ' + table_root)
 
-
-def insert_query(input_B, table_root):
-    with open(input_B + '.csv', newline='', encoding='utf-8') as csv_file:
+def insert_query(input_A, table_root):
+    with open(input_A + '.csv', newline='', encoding='utf-8') as csv_file:
         reader = csv.DictReader(csv_file)
         input_list = []
         '''
-        @col_headers --> Change column names here based on input csv file's columns,
+        @col_headers --> Change column names here based on input csv file's columns.
         '''
         col_headers = ['item_name', 'item_type', 'item_year', 'fb_annotation']
-        for row in reader: input_list.append([formatted_name(row(col_headers[0])), row(col_headers[0]),
-                                              row(col_headers[1]), row(col_headers[2]), row(col_headers[3])])
+        for row in reader:
+            input_list.append([formatted_name(row(col_headers[0])), row(col_headers[0]),
+                              row(col_headers[1]), row(col_headers[2]), row(col_headers[3])])
 
-        return ('INSERT INTO ' + table_root + '(' + ''
-                + ' VALUES ' + y)
+        '''
+    `   @db_col_headers --> Change column names here based on the databse columns to add values for.
+        '''
+        db_col_headers = ['item_name', 'item_type', 'item_year', 'item_timestamp']
+        db_col_headers_sql = '('
+        for i in range(len(db_col_headers)):
+            db_col_headers_sql += db_col_headers[i] + ','
+            if db_col_headers[i] == db_col_headers[-1]:
+                db_col_headers_sql = db_col_headers_sql[:-1] + ')'
+        return ('INSERT INTO ' + table_root + db_col_headers_sql + ' VALUES ' + y)
 
 
 def main():
     input_A = sys.argv[1]
-    input_B = ''
-    #TODO: 'WHERE' input as an additional customization for sql command
-    if input_A == 'select':
-        if sys.argv[2] != 'where':
-            arg_count = len(sys.argv)
-            for i in range(arg_count - 2): input_B += sys.argv[i + 2] + ', '
-            input_B = input_B[0:-2]  # Remove the last comma and space.
-        else: input_B = ''
-    elif input_A == 'insert':
-        input_B = sys.argv[2]
-    else:
-        print('Argument ERROR - First argument should only be "select" or "insert"')
-        exit(1)
-    table_query(input_A, input_B)
-
+    table_query(input_A)
 
 if __name__ == '__main__':
     main()
